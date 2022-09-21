c207e472990d0d940251596933b59dbce37e7713
DipoPY <dipoprog@gmail.com>
2022-09-21 20:58:16 +0300

    Laba1_Laba2

---
+++ Beluchenko_Dima_221_352/Beluchenko_Dima_221_352_lab1/Beluchenko_Dima_221_352_lab1.cpp
@@ -0,0 +1,50 @@
+﻿#include<iostream>
+#include<bitset>
+
+
+
+
+int main()
+{
+
+	setlocale(LC_ALL, "RUS");
+	std::cout << "1.\nБелюченко Дмитрий Николаевич, 221-352 " << std::endl;
+
+	std::cout << "2.\nmin int = " << int(0b10000000000000000000000000000000) << ", max int = " << int(0b01111111111111111111111111111111) << ", size of int = " << sizeof(int) << std::endl;
+	std::cout << "min unsigned int = " << "0" << ", max unsigned int = " << UINT_MAX << ", size of unsigned int = 4" << std::endl;
+	std::cout << "min short = " << SHRT_MIN << ", max short = " << SHRT_MAX << ", size of short = 2" << std::endl;
+	std::cout << "min unsigned short = " << "0" << ", max unsigned short = " << USHRT_MAX << ", size of unsigned short = 2" << std::endl;
+	std::cout << "min long = " << LONG_MIN << ", max long = " << LONG_MAX << ", size of long = 4 " << std::endl;
+	std::cout << "min long long = " << LLONG_MIN << ", max long long = " << LLONG_MIN << ", size of long long = 8" << std::endl;
+	std::cout << "min double = " << DBL_MIN << ", max double = " << DBL_MAX << ", size of double = 8" << std::endl;
+	std::cout << "min char =" << char(0b00000000) << ", max char = " << CHAR_MAX << ", size of char = " << sizeof(char) << std::endl;
+	std::cout << "min bool = " << "False" << ", max bool = " << "True" << ", size of bool = 1" << std::endl;
+
+
+
+	int digit = 0;
+	std::cout << "Введите число: ";
+	std::cin >> digit;
+
+	std::cout << std::bitset<sizeof(int) * 8>(digit) << std::endl;
+	std::cout << "bool " << bool(digit) << std::endl;
+	std::cout << "double " << double(digit) << std::endl;
+	std::cout << "char " << char(digit) << std::endl;
+
+	int a, x;
+	std::cout << "Введите коэффиценты a * x = b ";
+	std::cin >> a;
+	std::cin >> x;
+	std::cout << a << " * x = " << x << std::endl;
+	std::cout << "x = " << x << "/" << a << std::endl;
+	std::cout << "x = " << double(x) / a;
+
+	std::cout << "\n\n";
+
+	int g, b;
+	std::cout << "Введите координаты отрезка на прямой: ";
+	std::cin >> g;
+	std::cin >> b;
+	std::cout << "Середина отрезка находится в точке: " << (g + b) / 2.;
+
+}
---
+++ Beluchenko_Dima_221_352/Beluchenko_Dima_221_352_lab2/Beluchenko_Dima_221_352_lab2.cpp
@@ -0,0 +1,121 @@
+﻿#include<iostream>
+#include<string>
+
+double fac(int N)
+{
+	if (N < 0)
+	{
+		return 0;
+	}
+	if (N == 0)
+	{
+		return 1;
+	}
+	else
+	{
+		return N * fac(N - 1);
+	}
+}
+
+void task1()
+{
+	std::cout << "Задание 1." << std::endl;
+	int a;
+	std::cin >> a;
+	for (int i = 1; i <= a; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			std::cout << j;
+
+		}
+		std::cout << '\n';
+	}
+
+}
+
+void task2()
+{
+	int n, z;
+	std::cout << std::endl;
+	std::cout << "Задание 2." << std::endl;
+	std::cin >> n;
+	for (int k = 0; k <= n; k++)
+	{
+		z = n - k;
+		std::cout << "Бином = " << (fac(n)) / (fac(z) * fac(k)) << "\t k=" << k << "\n";
+
+	}
+	std::cout << "\n\n\n";
+}
+
+void task3()
+{
+	std::cout << std::endl;
+	std::cout << "Задание 3." << std::endl;
+	std::string end;
+	double total = 0, k;
+	end = "";
+	k = 0;
+	while (end != "end")
+	{
+		std::cout << "Введите: ";
+		std::cin >> end;
+		if (end != "end")
+		{
+			total += std::stod(end);
+			k++;
+		}
+	}
+	std::cout << float(total / k) << "\n\n\n";
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	while (true)
+	{
+
+
+		int m;
+		std::cout << "Что вы хотите выполнить? \n"
+			<< "1. задание 1\n"
+			<< "2. задание 2\n"
+			<< "3. задание 3\n"
+			<< "4. выход\n";
+		std::cin >> m;
+		switch (m)	
+		{
+		case 1:
+		{
+			task1();
+			break;
+		}
+		case 2:
+		{
+			task2();
+			break;
+		}
+
+
+
+		case 3:
+		{
+			task3();
+			break;
+		}
+		case 4:
+		{
+			return 0;
+		}
+
+
+		default:
+
+		{
+			return 0;
+		}
+
+		}
+	}
+}
